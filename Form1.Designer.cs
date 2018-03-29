7e0ea6bd0d5570b7c664522c82bee331598d31f0
Marcelo Pepis <marcelo.pepis@gmail.com>
2018-03-29 10:22:05 -0300

    0.1 Version
    Basics functionality, and bug free version.

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace QRCode_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1QrText = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1Qrc = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2SaveImage = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1Qrc)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Entre com o texto aqui:";
+            // 
+            // textBox1QrText
+            // 
+            this.textBox1QrText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1QrText.Location = new System.Drawing.Point(19, 59);
+            this.textBox1QrText.Multiline = true;
+            this.textBox1QrText.Name = "textBox1QrText";
+            this.textBox1QrText.Size = new System.Drawing.Size(353, 185);
+            this.textBox1QrText.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1Qrc);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(378, 59);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(305, 257);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Imagem QrCode";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // pictureBox1Qrc
+            // 
+            this.pictureBox1Qrc.Location = new System.Drawing.Point(55, 29);
+            this.pictureBox1Qrc.Name = "pictureBox1Qrc";
+            this.pictureBox1Qrc.Size = new System.Drawing.Size(214, 179);
+            this.pictureBox1Qrc.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1Qrc.TabIndex = 0;
+            this.pictureBox1Qrc.TabStop = false;
+            this.pictureBox1Qrc.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Gerar!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2SaveImage
+            // 
+            this.button2SaveImage.Location = new System.Drawing.Point(297, 273);
+            this.button2SaveImage.Name = "button2SaveImage";
+            this.button2SaveImage.Size = new System.Drawing.Size(75, 23);
+            this.button2SaveImage.TabIndex = 4;
+            this.button2SaveImage.Text = "Salvar";
+            this.button2SaveImage.UseVisualStyleBackColor = true;
+            this.button2SaveImage.Visible = false;
+            this.button2SaveImage.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 328);
+            this.Controls.Add(this.button2SaveImage);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1QrText);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "QrCode Generator - Megabarre";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1Qrc)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1QrText;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1Qrc;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2SaveImage;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿//Programa gerador de Qrcode para a empresa Megabarre
+//Feito por Marcelo de Paula Pepis
+//29/03/2018
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QRCode_Generator
+{
+    public partial class Form1 : Form
+    {
+        Image File;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {//eventos do botão gerar.
+            QRCoder.QRCodeGenerator qrGenerator = new QRCoder.QRCodeGenerator();
+            var qrData = qrGenerator.CreateQrCode(textBox1QrText.Text, QRCoder.QRCodeGenerator.ECCLevel.H);
+            var qrCode = new QRCoder.QRCode(qrData);
+            var image = qrCode.GetGraphic(150); //imagem gerada.
+            pictureBox1Qrc.Image = image; //colocando a imagem na caixa de imagens.
+            button2SaveImage.Visible = true;//depois de gerada a imagem, definimos o botão salvar como visivel.
+            File = pictureBox1Qrc.Image; //colocamos a imagem do qrcode em um arquivo do tipo Image.
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {//eventos do botao salvar.
+            SaveFileDialog f = new SaveFileDialog();
+            f.Filter = "JPEG(*.JPEG)|*.jpeg";//definimos um filtro de tipo de imagem, para salvarmos a imagem somente como JPEG.
+            if (f.ShowDialog() == DialogResult.OK) //criando a caixa de dialogos para savar.
+            {
+                File.Save(f.FileName);
+            }          
+        }
+    }
+}
